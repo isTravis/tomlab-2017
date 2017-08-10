804c7fc2cee4ca5983a5782558ae183612aed435
Travis Rich <travis.s.rich@gmail.com>
2017-08-10 19:15:29 -0400

    Add content for nat and sigin

--- app/content/natWare.js
+++ app/content/natWare.js
@@ -3,6 +3,35 @@ export default {
 	slug: 'nat-ware',
 	description: 'Tradable income-based securities (TIBS) to improve services',
 	title: 'Nat Ware',
+	email: 'nathanielware@gmail.com',
 	heroImage: 'nat',
 	essayPDF: 'http://www.symposium.org/sites/default/files/2017_Winner_Essay_Nat_Ware.pdf',
+	audience: ' impact investors, governments (especially finance ministers), education providers, investment banks, social finance organizations, reinsurance companies, vocational training providers, finance ministers, refugee advocacy organizations, and indigenous advocacy organizations',
+	html: `<div>
+		<h1>Tradeable Income-Based Securities</h1>
+		<h2>A New Way to Finance Services to Disadvantaged Populations Worldwide</h2>
+		<p>As we know, millions of refugees are being resettled across Europe. They need access to services – such as education and healthcare - to enable them to gain employment and achieve their potential.</p>
+		<p>However, governments are under political pressure to not spend too much taxpayer money on refugees. We don’t currently have a way to finance high-quality services to refugees at scale. It’s a seemingly intractable problem.</p>
+		<p>But what if I told you there’s a way to provide services that doesn’t cost the recipients anything, doesn’t rely on donations, doesn’t cost governments any money, and makes a profit for investors. It sounds too good to be true, but it’s not. </p>
+		<p>Tradable Income-Based Securities, or TIBS, is a new type of public-private partnership. The idea, in brief, is that governments award contracts to investors to provide services, such as vocational training, to consenting individuals. These services, by their nature, increase expected lifetime incomes, and therefore government tax revenue. Governments then pass on an agreed portion of that tax revenue to investors.</p>
+		<p>By way of example, suppose a resettled refugee wanted to be a plumber, but he had no money and no training. With TIBS, the government could award a contract to provide a two-year plumbing training course to 1000 refugees, in return for 20% of the lifetime tax revenue attributable to them.</p>
+		<p>Such an arrangement is mutually beneficial.</p>
+		<p>Individuals receive services at no additional cost. They just pay the usual tax rate. So rather than other taxpayers paying for refugees, refugees are effectively paying for their own investments with their future tax money. </p>
+		<p>As for governments, they pay nothing upfront, and there are ways of structuring the contract, so they only pass on in the future what they otherwise would not have had.</p>
+		<p>Investors can have a huge social impact, without needing to reduce or share profit. Since the securities are tradable, investors can profit in the short-term with uncapped upside.</p>
+		<p>With TIBS, there’s is no tradeoff between social and financial returns. Investors are incentivized to provide the highest quality services, because what maximizes social impact is also what maximizes profit. A true win-win.</p>
+		<p>This new type of public-private partnership can help millions of refugees. But more than that, it can also help the millions who will become unemployed and need retraining due to automation and deindustrialization. This is a way to finance that retraining at scale. What’s more, it can help to bridge the socio-economic divide between indigenous populations and the rest of the population, in many countries worldwide.</p>
+		<p>Overall, a key challenge in social finance and development economics is how to get stakeholders to work in the interests of disadvantaged populations. This idea, TIBS, attempts to do so by bringing together the government, investors, and disadvantaged individuals, in a mutually beneficial way. This is done by aligning their incentives, leveraging their different strengths, and working with the system we already have.</p>
+		<p>There are millions of people in need, and there are thousands of great ideas for how to help them – language lessons, hospitality training, coding courses. But many of those great ideas never happen, because they do not have a viable financing mechanism. This may be the missing link. This one idea could potentially enable thousands of great ideas to become reality. </p>
+		<p>The complete TIBS process is illustrated in the two diagrams below. </p>
+		<figure>
+			<img src="/images/nat/diagram1.jpg" />
+		</figure>
+		<figure>
+			<img src="/images/nat/diagram2.jpg" />
+		</figure>
+		<p>One final advantage of TIBS is that it enables a new market-based method of measuring social impact called Discounted Expected Marginal Impact (DEMI). DEMI uses the change in the present discounted value of expected lifetime income as a proxy for social impact. The change in the discounted expected lifetime income can be calculated from the change in the market price of a Tradeable Income-Based Security. Whereas the main existing method of measuring social impact, Social Return On Investment (SROI), assesses social return subjectively and intermittently relative to a no-investment counterfactual, DEMI is able to assesses social return objectively and continuously relative to a best alternative investment counterfactual. DEMI is able to measure marginal social impact rather than absolute social impact by ascertaining the counterfactual investment via a contract bidding process. </p>
+		<p>For more detailed information on how TIBS and DEMI would work, please contact Nat Ware at nathanielware@gmail.com.</p>
+
+	</div>`
 };
--- app/content/siginOjulu.js
+++ app/content/siginOjulu.js
@@ -3,6 +3,54 @@ export default {
 	slug: 'sigin-ojulu',
 	description: 'Strategic Virtual Task Simulations (SVTS) & Learning',
 	title: 'Sigin Ojulu',
+	email: 'OJULU@USC.EDU',
 	heroImage: 'sigin',
 	essayPDF: 'http://www.symposium.org/sites/default/files/2017_Winner_Essay_Sigin_Ojulu.pdf',
+	audience: 'Language Educators / Consultants / Translators Artists, 3D Modelers / Illustrators / Animators / Software Engineers / Coders Investors',
+	html: `<div>
+		<h1>The Culture Simulation (Prototype)</h1>
+		<h2>The Story</h2>
+		<p>When I first moved to California,  a few colleagues and took a drive to location scout for a film locations and also to test out our new drones. We had each only recently moved to Los Angeles as young professionals, yet one colleague Ben was oddly oriented and familiar with the terrain. When I asked him how he knew the geography so well, he replied by saying, "I know where I am because I played <em>Grand Theft Auto</em> – it’s the same exact topography." The other colleague Alex chimed in saying, "Yeah, I wouldn’t have felt as comfortable in Miami if my mom didn’t let me play <em>Vice City</em>". Oddly enough, I had never operated a drone before this expedition and I certainly was not a huge gamer before getting into virtual reality (VR), but because my thumbs and technological expectations are wired for hardware to be interactive, the first time that I flew a drone was incredibly simple and felt natural to me. </p>
+		<p>What does this tell us? It seems to me that a great deal of today’s global issues, at a very rudimentary base, come from a lack of understanding – not because we do not possess the information to understand, rather because the way in which we acquire and learn knowledge is highly partial, cumbersome and inefficient. There is a technological paradox; each generation receives more and more information and we have the capabilities to connect with more and more people, yet at the same time, technology makes us more insular and solidifies the beliefs of our milieus in a way that isolates others that are different from us.  Many of our current cognitive comprehensions fall into this paradox and beg us to address the always-looming role of technology (as we should). However, on a more fundamental level, I might propose that we begin to question the role and practice of our learning methods, particularly in our educational institutions. Overall, our global educational structures of curriculum are archaic. General learning structures tend to be uni-dimensional and focused on skill-building and particular professional trajectories. Educational institutions are also where we develop a societal collective conscious that solidifies our differentiated identity to the global world. Contextualization falls to the wayside even within multi-disciplinary approaches, and this continuously leads us to insular knowledge regardless of the copious amounts of information which technology affords us daily. Our knowledge is no longer contextualized, rather solidified and this is visibly extrapolated into the increasingly polarized ideologies of our world.</p>
+		<p>Now, if gaming is powerful enough to elicit in Ben an employable mental map of a place he had never physically been, certainly this is a minute illustration of the power of interactive learning methods; thus urging us in the direction of drastic reconstruction of learning practices. Virtual reality provides a platform in which we can develop our learning (and by extension understanding) amidst the abundance of biased information we receive daily.	</p>
+		<h2>The Project</h2>
+		<p>The prototype we are developing is an educational (VR) simulation in which we can learn about other cultures through interactive gameplay. The pilot content countries we are looking to develop are in Egypt, France, Spain, Senegal & China. Let’s start off in Egypt.</p>
+		<iframe src="https://drive.google.com/file/d/0B1OCLC4RSIvEdVpPV1FheGF1S1U/preview" width="640" height="480"></iframe>
+		<h2>The Design</h2>
+		<p>The Prototype Design is as follows:</p>
+		<em>*Video & pictures are un-rendered references to the design. The final product hopes to look like live action gaming.</em>
+		<h3>Phase 1: Choose your country</h3>
+		<figure>
+			<img src="/images/sigin/image01.png" />
+		</figure>
+		<figure>
+			<iframe src="https://drive.google.com/file/d/0B1OCLC4RSIvETm9OX1c0M0NIYlU/preview" width="640" height="480"></iframe>
+		</figure>
+		<p>You as the gamer will be fully immersed and interact with the content that you chose to engage with.</p>
+		<figure>
+			<img src="/images/sigin/image02.png" />
+		</figure>
+		<figure>
+			<iframe src="https://drive.google.com/file/d/0B1OCLC4RSIvEVVdUY2FDLXlObkE/preview" width="640" height="480"></iframe>
+		</figure>
+		<p>Each artifact will lead you to a new segment of the world that you have chosen.</p>
+		<p>Going to school, for example, will launch you into a new interactive game design that will teach you Arabic like Rosetta Stone. But instead of being trapped inside a book or behind a screen, it will be like you are in the middle of the country. You will interact with avatars who speak the language and take further field trips to learn vocabulary, grammar, colloquialisms of Arabic, etc.</p>
+		<figure>
+			<img src="/images/sigin/image03.png" />
+		</figure>
+		<figure>
+			<iframe src="https://drive.google.com/file/d/0B1OCLC4RSIvEWTFPVjN0THE3VzQ/preview" width="640" height="480"></iframe>
+		</figure>
+		<p>Choose your own content. Behind each artifact is a story, an experience or lesson to help you better understand the content and furthermore, encode the information you have just learned better.</p>
+		<figure>
+			<img src="/images/sigin/image04.png" />
+		</figure>
+		<figure>
+			<iframe src="https://drive.google.com/file/d/0B1OCLC4RSIvEMldIeHZ4bk94Zms/preview" width="640" height="480"></iframe>
+		</figure>
+		<figure>
+			<img src="/images/sigin/image05.png" />
+		</figure>
+
+	</div>`
 };
