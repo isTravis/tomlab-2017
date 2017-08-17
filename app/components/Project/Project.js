5a068da6f73fc8a3a9ffbd83c0f0c22080298fd8
Travis Rich <travis.s.rich@gmail.com>
2017-08-17 11:50:41 -0400

    Test local video

--- app/components/Project/Project.js
+++ app/components/Project/Project.js
@@ -75,19 +75,21 @@ const Project = function(props) {
 				<div className={'row'}>
 					<div className={'col-12'}>
 						<div className={'left-padded'}>
-							<div className={'audience'}>
-								{projectData.audience.map((item, index)=> {
-									return <span className={'project-audience-item'} key={`audience-${projectData.slug}-${index}`}>{item} {index !== 0 && index !== projectData.audience.length -1 && '·'} </span>;
-								})}
+							<div className={'cta-container'}>
+								<div className={'audience'}>
+									{projectData.audience.map((item, index)=> {
+										return <span className={'project-audience-item'} key={`audience-${projectData.slug}-${index}`}>{item} {index !== 0 && index !== projectData.audience.length -1 && '·'} </span>;
+									})}
+								</div>
+								<a href={`mailto:${projectData.email}`} className={`contact-button ${mode}`}>Message {projectData.firstName}</a>
+								<img alt={projectData.title} className={'bio-pic'} src={`/images/${projectData.bioPic}`} />
+								<div className={'bio-text'}>{projectData.bioText}</div>
+								<ul className={'link-list'}>
+									<a href={projectData.pitchVideoUrl}><li>Pitch (3min video)</li></a>
+									<a href={projectData.essayPDF}><li>Essay (PDF)</li></a>
+									<a href={projectData.linkedinUrl}><li>LinkedIn Profile</li></a>
+								</ul>
 							</div>
-							<a href={`mailto:${projectData.email}`} className={`contact-button ${mode}`}>Message {projectData.firstName}</a>
-							<img alt={projectData.title} className={'bio-pic'} src={`/images/${projectData.bioPic}`} />
-							<div className={'bio-text'}>{projectData.bioText}</div>
-							<ul className={'link-list'}>
-								<a href={projectData.pitchVideoUrl}><li>Pitch (3min video)</li></a>
-								<a href={projectData.essayPDF}><li>Essay (PDF)</li></a>
-								<a href={projectData.linkedinUrl}><li>LinkedIn Profile</li></a>
-							</ul>
 
 						</div>
 					</div>
