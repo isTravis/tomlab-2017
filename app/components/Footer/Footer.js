ea5e0114664e510e0184c5c242abf98dfbc1ddc8
Travis Rich <travis.s.rich@gmail.com>
2017-08-22 10:09:04 -0400

    Test external click 1

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,14 +1,23 @@
+/* global trackOutboundLink */
+
 import React from 'react';
 
 require('./footer.scss');
 
 const Footer = function() {
+	const outLink = function(url) {
+		return ()=> {
+			trackOutboundLink(url);
+			return false;
+		};
+	};
+
 	return (
 		<nav className={'footer'}>
 			<div className={'container'}>
 				<div className={'row'}>
 					<div className={'col-8'}>
-						<p className={'text'}>TOM-LAB 2017 facilitated by <a href={'https://www.raphaelschaad.com/'} onClick={()=>{ trackOutboundLink('https://www.raphaelschaad.com/'); return false; }} target={'_blank'}>Raphael Schaad</a>, <a href={'https://media.mit.edu'} onClick={()=>{ trackOutboundLink('https://media.mit.edu'); return false; }} target={'_blank'}>MIT Media Lab</a>. Site hosted with <a href={'https://www.pubpub.org'} onClick={()=>{ trackOutboundLink('https://www.pubpub.org'); return false; }} target={'_blank'}>PubPub</a>.</p>
+						<p className={'text'}>TOM-LAB 2017 facilitated by <a href={'https://www.raphaelschaad.com/'} onClick={outLink('https://www.raphaelschaad.com/')} target={'_blank'}>Raphael Schaad</a>, <a href={'https://media.mit.edu'} onClick={outLink('https://media.mit.edu')} target={'_blank'}>MIT Media Lab</a>. Site hosted with <a href={'https://www.pubpub.org'} onClick={outLink('https://www.pubpub.org')} target={'_blank'}>PubPub</a>.</p>
 					</div>
 				</div>
 			</div>
--- app/indexProd.js
+++ app/indexProd.js
@@ -41,8 +41,7 @@ export default function(locals) {
 			*/
 			var trackOutboundLink = function(url) {
 				ga('send', 'event', 'outbound', 'click', url, {
-					'transport': 'beacon',
-					'hitCallback': function(){document.location = url;}
+					'transport': 'beacon'
 				});
 			}
 			</script>
